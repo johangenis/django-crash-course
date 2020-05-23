48303a26279a08d56f2064f9692856dc29cbd885
johangenis <johan.j.genis@gmail.com>
2020-05-23 12:56:49 +0200

    Todo Detail View.

--- crashcourse/todo/urls.py
+++ crashcourse/todo/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import todo_list
+from .views import todo_list, todo_detail
 
 app_name = "todos"
 
 
-urlpatterns = [path("", todo_list)]
+urlpatterns = [path("", todo_list), path("<id>", todo_detail)]
--- crashcourse/todo/views.py
+++ crashcourse/todo/views.py
@@ -5,6 +5,14 @@ from .models import Todo
 
 def todo_list(request):
     todos = Todo.objects.all()
-    print(todos)
     context = {"todo_list": todos}
     return render(request, "todo_list.html", context)
+
+
+# CRUD - Create, Retrieve, Update, Delete, List
+
+
+def todo_detail(request, id):
+    todo = Todo.objects.get(id=id)
+    context = {"todo": todo}
+    return render(request, "todo_detail.html", context)
