6738ca1d4e8cf98bf6be732770934977ee53482d
johangenis <johan.j.genis@gmail.com>
2020-05-23 12:00:35 +0200

    Added Todo List View and Template.

--- crashcourse/crashcourse/urls.py
+++ crashcourse/crashcourse/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from todo.views import todo_list
 
-urlpatterns = [path("admin/", admin.site.urls), path("", todo_list)]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("todo.urls", namespace="todos")),
+]
